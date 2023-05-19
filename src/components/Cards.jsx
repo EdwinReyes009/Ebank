8c02b3a2446302e2de4285b8e40df8af387038ed
angelzd27 <angeltj27@gmail.com>
2023-05-18 19:00:54 -0600

    components finished

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import { card, quotes } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
 
-export const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+export const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/> in few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>This card is made for you because we thought in you and all the things that you cand od with this card in your pocket.</p>
 
-export const CardFeedback = () => {
-  return (
-    <div>CardFeedback</div>
-  )
-}
+      <Button styles="mt-10" text="Get Started"/>
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt='card' className='w-[100%] h-[100%]'></img>
+    </div>  
+  </section>
+)
+
+export const FeedbackCard = ({ content, name, title, img }) => (
+  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[350px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+    <img src={quotes} alt="double_quotes" className="w-[42.6px] h-[27.6px] object-contain" />
+    <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
+      {content}
+    </p>
+
+    <div className="flex flex-row">
+      <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full" />
+      <div className="flex flex-col ml-4">
+        <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">
+          {name}
+        </h4>
+        <p className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite">
+          {title}
+        </p>
+      </div>
+    </div>
+  </div>
+);
